98e8a5ada67f1a60530f3263e1ce0cd0e55f7a38
Furkan Coşar <furkancosar2005@gmail.com>
2025-07-26 02:43:46 +0300

    css hotfix

--- assets/js/hesapmakinesi.js
+++ assets/js/hesapmakinesi.js
@@ -1,15 +1,15 @@
-let result = document.querySelector('.screen h1');
-let btns = document.querySelectorAll('.process button');
-let sonucBtn = document.querySelector('.process-end .esittir');
-let silme = document.querySelector('button.btn.btn-del');
-let sifirlama = document.querySelector('.process-end .sifirlama');
+let result = document.querySelector(".screen h1");
+let btns = document.querySelectorAll(".process button");
+let sonucBtn = document.querySelector(".process-end .esittir");
+let silme = document.querySelector("button.btn.btn-del");
+let sifirlama = document.querySelector(".process-end .sifirlama");
 
 function handleBtnClick() {
-  result.innerText += this.innerText 
+  result.innerText += this.innerText;
 }
 
-for(let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', handleBtnClick);
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", handleBtnClick);
 }
 
 function handleSonucClick() {
@@ -17,9 +17,9 @@ function handleSonucClick() {
 }
 
 function handleDelete() {
-  result.innerText = ' ';
+  result.innerText = " ";
 }
 
-sifirlama.addEventListener('click', handleDelete);
-silme.addEventListener('click', handleDelete);
-sonucBtn.addEventListener('click', handleSonucClick);
+sifirlama.addEventListener("click", handleDelete);
+silme.addEventListener("click", handleDelete);
+sonucBtn.addEventListener("click", handleSonucClick);
